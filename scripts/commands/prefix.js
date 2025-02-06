635f8c35ec43e5ac88a2936e47a32650cda3d0e6
VEX_ADNAN-404 <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2025-02-06 09:19:35 +0600

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -25,9 +25,9 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 		const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (config.PREFIX == null) {
-        return out(`bot prefix : ${global.config.PREFIX}`)
+        return out(`𝗜𝗗𝗢𝗟𝗟 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫 : ${global.config.PREFIX}`)
       }
-      else return out(`bot prefix : ${global.config.PREFIX}`)
+      else return out(`𝗜𝗗𝗢𝗟𝗟 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫 : ${global.config.PREFIX}`)
     }
 
   });
